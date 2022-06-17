9802e22763d6d60f5ee490667741feb8c0d9bebd
BanjiBear <tommychen900217@gmail.com>
2022-06-18 04:01:19 +0800

    Step 1

--- src/Controller.java
+++ src/Controller.java
@@ -4,14 +4,34 @@
     read write json file
     https://howtodoinjava.com/java/library/json-simple-read-write-json-examples/
 */
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
-//import org.json.simple.JSONArray;
-//import org.json.simple.JSONObject;
-//import org.json.simple.parser.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Controller {
-    
+
+    private static String[] splitString;
+
+    public static void main(String[] args) {
+        Path fileName = Path.of("train.txt");
+        String trainTexts = null;
+        try {
+            trainTexts = Files.readString(fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //System.out.println(trainTexts);
+        // Reference: https://www.geeksforgeeks.org/java-program-to-read-a-file-to-string/
+
+        splitString = sentenceSegmentation(trainTexts);
+    }
+
+    private static String[] sentenceSegmentation(String trainTexts){
+        String delimiter = "[.?!]";
+        return (trainTexts.split(delimiter));
+    }
+
 }
